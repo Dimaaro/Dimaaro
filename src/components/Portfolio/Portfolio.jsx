94f7947346c95b5dff46855cf65b8086d066e3c6
Dima <dimaaro@icloud.com>
2023-06-29 00:54:30 +0200

    Sumarry

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,32 +2,32 @@ import Image from "next/image";
 import styles from "./portfolio.module.css";
 import Link from "next/link";
 
-import portfolio from "./data.js";
+import data from "./data.js";
 
 export default function Portfolio() {
-  console.log(portfolio[1]);
-
   return (
     <section className={styles.container}>
       <h2 className={styles.h2}>Projekte</h2>
       <div className={styles.items}>
-        <div className={styles.item}>
-          <Link href="#">
-            <div className={styles.imgContainer}>
-              <Image
-                src="/existenzstand.svg"
-                fill={true}
-                className={styles.img}
-              />
-            </div>
-            <div className={styles.linkContainer}>
-              <div className={styles.icon}>
-                <Image src="/link.svg" width={8.7} height={8.7} />
+        {data.map((item) => (
+          <div key={item.id} className={styles.item}>
+            <Link href={item.url}>
+              <div className={styles.imgContainer}>
+                <Image
+                  src={item.image}
+                  fill={true}
+                  className={styles.img}
+                />
               </div>
-              <div className={styles.link}>ExistenStand</div>
-            </div>
-          </Link>
-        </div>
+              <div className={styles.linkContainer}>
+                <div className={styles.icon}>
+                  <Image src="/link.svg" width={8.7} height={8.7} />
+                </div>
+                <div className={styles.link}>{ item.name }</div>
+              </div>
+            </Link>
+          </div>
+        ))}
       </div>
     </section>
   );
--- src/components/Portfolio/data.js
+++ src/components/Portfolio/data.js
@@ -1,33 +1,36 @@
 const portfolio = [
-  {
-    id: 1,
-    name: "ExistenzStand GmbH",
-    url: "",
-    image: ""
-  },
-  {
-    id: 2,
-    name: "IBO E-Technik GmbH",
-    url: "https://elektriksoest.de",
-    image: ""
-  },
-  {
-    id: 3,
-    name: "Consulution GmbH",
-    url: "https://consulution.de",
-    image: ""
-  },
-  {
-    id: 4,
-    name: "Ambulanter Pflegedienst",
-    url: "https://gesundheitspartner-bergkamen.de",
-    image: ""
-  },
-  {
-    id: 5,
-    name: "TuS Lintfort",
-    url: "https://tus-lintfort.de",
-    image: ""
-  },
-  
+    {
+      id: 1,
+      name: "ExistenzStand GmbH",
+      url: "https://existenzstand.de",
+      image: "/existenzstand.svg"
+    },
+    {
+      id: 2,
+      name: "IBO E-Technik GmbH",
+      url: "https://elektriksoest.de",
+      image: "/iboetechnik.svg"
+    },
+    {
+      id: 3,
+      name: "Consulution GmbH",
+      url: "https://consulution.de",
+      image: "consulution.svg"
+    },
+    {
+      id: 4,
+      name: "Ambulanter Pflegedienst",
+      url: "https://gesundheitspartner-bergkamen.de",
+      image: "gesundheitspartnerbergkamen.svg"
+    },
+    {
+      id: 5,
+      name: "TuS Lintfort",
+      url: "https://tus-lintfort.de",
+      image: "/tuslintfort.svg"
+    },
+    
 ]
+  
+export default portfolio
+  
