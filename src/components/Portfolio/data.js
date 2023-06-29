1eb63b6bc9d8319cb318464051ced45600f2d818
Dima <dimaaro@icloud.com>
2023-06-29 11:30:30 +0200

    Summary

--- src/components/Portfolio/data.js
+++ src/components/Portfolio/data.js
@@ -3,31 +3,31 @@ const portfolio = [
       id: 1,
       name: "ExistenzStand GmbH",
       url: "https://existenzstand.de",
-      image: "/existenzstand.svg"
+      image: "/existenzstand.png"
     },
     {
       id: 2,
       name: "IBO E-Technik GmbH",
       url: "https://elektriksoest.de",
-      image: "/iboetechnik.svg"
+      image: "/iboetechnik.png"
     },
     {
       id: 3,
       name: "Consulution GmbH",
       url: "https://consulution.de",
-      image: "consulution.svg"
+      image: "/consulution.png"
     },
     {
       id: 4,
       name: "Ambulanter Pflegedienst",
       url: "https://gesundheitspartner-bergkamen.de",
-      image: "gesundheitspartnerbergkamen.svg"
+      image: "/gesundheitspartnerbergkamen.png"
     },
     {
       id: 5,
       name: "TuS Lintfort",
       url: "https://tus-lintfort.de",
-      image: "/tuslintfort.svg"
+      image: "/tuslintfort.png"
     },
     
 ]
